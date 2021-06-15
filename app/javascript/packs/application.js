6f1f1498e6cdab8125a390b5e4dd004c13af3ff4
Jacob Swanner <jacob@envylabs.com>
2021-06-15 13:32:20 -0700

    6.1.0.rc2

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,10 +11,3 @@ import "channels"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
-
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
