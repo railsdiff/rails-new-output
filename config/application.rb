8b6cd8f48f332b3bbb5e6c8b41f678c409638d55
Jacob Swanner <jacob@envylabs.com>
2021-06-15 13:31:58 -0700

    3.1.0.rc1

--- config/application.rb
+++ config/application.rb
@@ -37,15 +37,11 @@ module Railsdiff
     # JavaScript files you want as :defaults (application.js is always included).
     # config.action_view.javascript_expansions[:defaults] = %w(prototype prototype_ujs)
 
-
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
-
-    # Enable IdentityMap for Active Record, to disable set to false or remove the line below.
-    config.active_record.identity_map = true
 
     # Enable the asset pipeline
     config.assets.enabled = true
