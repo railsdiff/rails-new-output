2c5693272242fdc8a8f27812be3c99bfcbd35e08
Jacob Swanner <jacob@envylabs.com>
2021-06-15 13:32:20 -0700

    6.1.0.rc1

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,11 +3,14 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
+import Rails from "@rails/ujs"
+import Turbolinks from "turbolinks"
+import * as ActiveStorage from "@rails/activestorage"
+import "channels"
 
+Rails.start()
+Turbolinks.start()
+ActiveStorage.start()
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
