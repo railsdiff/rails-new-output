94199e402d2bd4d08627dd5b855c3cce7edce924
Jacob Swanner <jacob@envylabs.com>
2021-06-15 13:32:04 -0700

    4.0.0.rc1

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Railsdiff::Application.config.session_store :encrypted_cookie_store, key: '_railsdiff_session'
+Railsdiff::Application.config.session_store :cookie_store, key: '_railsdiff_session'
